b6536ac72d6b416fbe2f583f77a2bf05fc5d4a7d
Jamal Hassouni <jamalhassouni13@gmail.com>
2018-10-02 00:02:12 +0100

    fix:Fix Firebase error: Object are not valid as a React child

--- src/firebase.js
+++ src/firebase.js
@@ -1,12 +1,14 @@
-import * as firebase from 'firebase';
+import firebase from "@firebase/app";
+import "@firebase/auth";
 
+// Initialize Firebase
 const config = {
-    apiKey: 'API_KEY',
-    authDomain: 'AUTH_DOMAIN_HERE',
-    databaseURL: 'DB_URL_HERE',
-    projectId: 'PROJECT_ID',
-    storageBucket: 'STORAGE_BUCKER',
-    messagingSenderId: 'SENDER_ID'
+  apiKey: 'API_KEY',
+  authDomain: 'AUTH_DOMAIN_HERE',
+  databaseURL: 'DB_URL_HERE',
+  projectId: 'PROJECT_ID',
+  storageBucket: 'STORAGE_BUCKER',
+  messagingSenderId: 'SENDER_ID'
 };
 
 firebase.initializeApp(config);
